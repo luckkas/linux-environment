adff57fd0670698fe58d50acad9c0d577a57a3a5
luckkas <luckasmelo.dev@gmail.com>
2024-04-06 10:34:23 -0300

    feat: added script to install dnf apps (fedora)

---
+++ scripts/install_dnf_apps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+install_dnf_programs() {
+    echo "Installing programs using DNF..."
+    sudo dnf install -y "$@"
+}
+
+main() {
+    if ! command_exists dnf; then
+        echo "DNF is not installed. Please install DNF and try again."
+        exit 1
+    fi
+
+    programs=(
+        btrfs-assistant
+    )
+
+    install_dnf_programs "${programs[@]}"
+}
+
+# Run the main function
+main
